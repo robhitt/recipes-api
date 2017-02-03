fc5e71869ac34eec1e7abbcebbf03c82bd5815c4
Rob Hitt <robhitt@gmail.com>
2017-02-03 16:04:43 -0500

    add view functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # ADD HTML ROUTES HERE
+  resources :recipes
+
+  # API ROUTES
   scope module: 'api' do
     namespace :v1 do
       resources :recipes
